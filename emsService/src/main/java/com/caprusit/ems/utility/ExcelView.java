4deeb3d8193d642b1c286cd54c0ed5efe54432d8
rahul.murlidhar <rahul.nichal12@gmail.com>
2015-12-16 12:00:21 +0530

    Commit excelView file.

---
+++ emsService/src/main/java/com/caprusit/ems/utility/ExcelView.java
@@ -0,0 +1,63 @@
+package com.caprusit.ems.utility;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.springframework.web.servlet.view.document.AbstractExcelView;
+
+import com.caprusit.ems.domain.Employee;
+
+@SuppressWarnings("deprecation")
+public class ExcelView extends AbstractExcelView {
+	@Override
+	protected void buildExcelDocument(@SuppressWarnings("rawtypes") Map model, 
+			                                                                    HSSFWorkbook workbook,
+			                                                                    HttpServletRequest request,
+			                                                                    HttpServletResponse response) throws Exception {
+
+		Logger logger = Logger.getLogger(getClass().getName());
+		@SuppressWarnings("unchecked")
+		List<Employee> employees = (List<Employee>) model.get("employees");
+		logger.info("in ExcelView utility :"+ employees);
+		response.setContentType("application/vnd.ms-excel");
+		HSSFSheet sheet = workbook.createSheet("Employee Report");
+		logger.info("sheet created"+sheet.getSheetName());
+		HSSFRow header = sheet.createRow(0);
+		header.createCell(0).setCellValue("Employee Id");
+		header.createCell(1).setCellValue("First Name");
+		header.createCell(2).setCellValue("Last Name");
+		header.createCell(3).setCellValue("Date of Birth");
+		header.createCell(4).setCellValue("Mobile No");
+		header.createCell(5).setCellValue("Email Id");
+		header.createCell(6).setCellValue("Designation");
+		header.createCell(7).setCellValue("Role Id");
+		header.createCell(8).setCellValue("Status");
+		header.createCell(9).setCellValue("Department Id");
+
+		logger.info("sheet  header created"+header.getSheet());
+		int counter = 1;
+		for (Employee e : employees) {
+			HSSFRow row = sheet.createRow(counter++);
+			row.createCell(0).setCellValue(e.getEmployeeId());
+			row.createCell(1).setCellValue(e.getFirstName());
+			row.createCell(2).setCellValue(e.getLastName());
+			logger.info(e.getDob()+" DOB");
+			row.createCell(3).setCellValue(""+e.getDob());
+			row.createCell(4).setCellValue(""+e.getMobileNo());
+			row.createCell(5).setCellValue(e.getEmailId());
+			row.createCell(6).setCellValue(e.getDesignation());
+			row.createCell(7).setCellValue(""+e.getRollId());
+			row.createCell(8).setCellValue(""+e.getStatus());
+			row.createCell(9).setCellValue(""+e.getDeptId());
+
+		}
+	}
+
+}
